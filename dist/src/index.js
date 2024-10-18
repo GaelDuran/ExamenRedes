2902e14dfe6a5499a26c731fd0521670d0dfd056
GaelDuran <ldt76578@lasallebajio.edu.mx>
2024-10-18 10:58:38 -0600

    .

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,20 +1,42 @@
-const express = require('express');
-const { dbConnection } = require('./db/init');
-const { User } = require('./db/models/user.model');
-const { comparePassword, hashPassword } = require('./utils/utils');
-const app = express();
-app.use(express.json());
-dbConnection();
-app.get("/", async (req, res) => {
-    res.json({ message: "Respondiendo al GET" });
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const sequelize_1 = require("sequelize");
+// Crea la conexión a la base de datos usando las variables de entorno
+const sequelize = new sequelize_1.Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
 });
-app.post("/", async (req, res) => {
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+// Verifica la conexión a la base de datos
+sequelize.authenticate()
+    .then(() => {
+    console.log('Conexión a la base de datos establecida con éxito.');
+})
+    .catch((err) => {
+    console.error('No se pudo conectar a la base de datos:', err);
+});
+app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    res.send("Hello World!");
+}));
+app.post("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const body = req.body;
     console.log(body);
     res.json({ message: "Body leído" });
-});
-app.post("/login", async (req, res) => {
-});
+}));
 app.listen(3000, () => {
     console.log("Aplicación corriendo en el puerto 3000");
 });
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
 import express, { Request, Response } from 'express';
-import { dbConnection } from './db/init';
-import { Todo } from './db/models/user.models';
-import { comparePassword, hashPassword } from './utils/utils';
+import { Sequelize } from 'sequelize';
+
+// Crea la conexión a la base de datos usando las variables de entorno
+const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASSWORD!, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+});
 
 const app = express();
 app.use(express.json());
 
-dbConnection();
+// Verifica la conexión a la base de datos
+sequelize.authenticate()
+  .then(() => {
+    console.log('Conexión a la base de datos establecida con éxito.');
+  })
+  .catch((err) => {
+    console.error('No se pudo conectar a la base de datos:', err);
+  });
 
 app.get("/", async (req: Request, res: Response) => {
   res.send("Hello World!");
@@ -16,10 +27,6 @@ app.post("/", async (req: Request, res: Response) => {
   const body = req.body;
   console.log(body);
   res.json({ message: "Body leído" });
-});
-
-app.post("/login", async (req: Request, res: Response) => {
-  // Lógica para el manejo de login
 });
 
 app.listen(3000, () => {
